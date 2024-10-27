e5b4c2d8bbf6c92f8033b33f77ca5e895fce3670
Ferchulop <152642994+Ferchulop@users.noreply.github.com>
2024-10-27 21:15:40 +0100

    Add struct Activity and class Activities with protocol @Observable

--- Habit-Tracking/ContentView.swift
+++ Habit-Tracking/ContentView.swift
@@ -6,7 +6,18 @@
 //
 
 import SwiftUI
-
+struct Activity: Identifiable, Codable, Equatable {
+    let id = UUID()
+    let name: String
+    let description: String
+    let isCompleted: Bool
+}
+// Uso el protocolo @Observable para monitorear los datos en caso de cambios.
+@Observable
+class Activities {
+    
+    
+}
 struct ContentView: View {
     var body: some View {
         VStack {
